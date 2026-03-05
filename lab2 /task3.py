9a692b1773dcaf04a9585928b8168f6ecfed2fd6
sudenklc <sudeklc6@gmail.com>
2026-03-05 13:46:48 +0300

    Update task3.py

--- lab2 /task3.py
+++ lab2 /task3.py
@@ -1,5 +1,9 @@
 n = int(input("Enter a positive integer between 3 and 9 inclusive: "))
 
+while n < 3 or n > 9:
+    print("Invalid number. Please enter a number between 3 and 9.")
+    n = int(input("Please enter a number between 3 and 9: "))
+
 for i in range(1, 2 * n):
     k = n - abs(n - i)
 

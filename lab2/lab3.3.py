5110b246d9d135e2a684b6542d2aa4b125004562
sudenklc <sudeklc6@gmail.com>
2026-03-05 13:48:04 +0300

    Delete lab2 directory

--- lab2/lab3.3.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a positive integer between 3 and 9 inclusive: "))
-
-for i in range(1, 2 * n):
-    k = n - abs(n - i)
-
-    for j in range(k):
-        print("*", end=" ")
-
-    print()

439858aa4ab29599b910ee47a24bc9f8b6d2d8a7
sudenklc <sudeklc6@gmail.com>
2026-04-16 13:37:58 +0300

    Add functions to calculate mean, max, and min

---
+++ lab6/data_package/analyzer.py
@@ -0,0 +1,12 @@
+def calculate_mean(num_list):
+    if len(num_list) == 0:
+        return 0
+    return sum(num_list) / len(num_list)
+
+
+def find_maximum(num_list):
+    return max(num_list)
+
+
+def find_minimum(num_list):
+    return min(num_list)
